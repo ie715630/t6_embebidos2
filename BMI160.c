e8245467a0997f1899b75225adbb51ae2ab502cf
Sergio Mendez <39496193+sergio-mndz@users.noreply.github.com>
2020-10-15 20:27:43 -0500

    Fixed data transfer

--- BMI160.c
+++ BMI160.c
@@ -7,6 +7,8 @@
 
 #define BMI160_PRINTF_OFF 1
 
+#include "BMI160.h"
+
 void bmi160_I2C_initialization(void *args)
 {
 	parameters_task_t parameters_task = *((parameters_task_t*)args);
@@ -34,24 +36,18 @@ void bmi160_I2C_initialization(void *args)
 bmi160_raw_data_t bmi160_i2c_read_acc(void)
 {
 	bmi160_raw_data_t data_acc;
+	uint64_t raw_acc_data = 0;
+	uint16_t x_data;
+	uint16_t y_data;
+	uint16_t z_data;
 
-	uint8_t data_x_lo = 0;
-	uint8_t data_x_hi = 0;
-	uint8_t data_y_lo = 0;
-	uint8_t data_y_hi = 0;
-	uint8_t data_z_lo = 0;
-	uint8_t data_z_hi = 0;
-
-	i2c_multiple_read(BMI160_SLAVE_ADDR, 0, reg_acc_x_lo , 1);
-	i2c_multiple_read(BMI160_SLAVE_ADDR, 0, reg_acc_x_hi , 1);
-	i2c_multiple_read(BMI160_SLAVE_ADDR, 0, reg_acc_y_lo , 1);
-	i2c_multiple_read(BMI160_SLAVE_ADDR, 0, reg_acc_y_hi , 1);
-	i2c_multiple_read(BMI160_SLAVE_ADDR, 0, reg_acc_z_lo , 1);
-	i2c_multiple_read(BMI160_SLAVE_ADDR, 0, reg_acc_z_hi , 1);
-
-	data_acc.x = (data_x_hi << 8) + data_x_lo;
-	data_acc.y = (data_y_hi << 8) + data_y_lo;
-	data_acc.z = (data_z_hi << 8) + data_z_lo;
+	i2c_multiple_read(BMI160_SLAVE_ADDR, reg_acc_x_lo, &raw_acc_data , 6);
+	x_data = raw_acc_data & 0xffff;
+	y_data = (raw_acc_data >> 16) & 0xffff;
+	z_data = (raw_acc_data >> 32) & 0xffff;
+	data_acc.x = x_data;
+	data_acc.y = y_data;
+	data_acc.z = z_data;
 
 #ifndef BMI160_PRINTF_OFF
 	PRINTF("BMI160 - Accel en x: %d \n", data_acc.x);
@@ -64,24 +60,17 @@ bmi160_raw_data_t bmi160_i2c_read_acc(void)
 
 bmi160_raw_data_t bmi160_i2c_read_gyr(void) {
 	bmi160_raw_data_t data_gyro;
-
-	uint8_t data_x_lo = 0;
-	uint8_t data_x_hi = 0;
-	uint8_t data_y_lo = 0;
-	uint8_t data_y_hi = 0;
-	uint8_t data_z_lo = 0;
-	uint8_t data_z_hi = 0;
-
-	i2c_multiple_read(BMI160_SLAVE_ADDR, 0, reg_gyro_x_lo , 1);
-	i2c_multiple_read(BMI160_SLAVE_ADDR, 0, reg_gyro_x_hi , 1);
-	i2c_multiple_read(BMI160_SLAVE_ADDR, 0, reg_gyro_y_lo , 1);
-	i2c_multiple_read(BMI160_SLAVE_ADDR, 0, reg_gyro_y_hi , 1);
-	i2c_multiple_read(BMI160_SLAVE_ADDR, 0, reg_gyro_z_lo , 1);
-	i2c_multiple_read(BMI160_SLAVE_ADDR, 0, reg_gyro_z_hi , 1);
-
-	data_gyro.x = (data_x_hi << 8) + data_x_lo;
-	data_gyro.y = (data_y_hi << 8) + data_y_lo;
-	data_gyro.z = (data_z_hi << 8) + data_z_lo;
+	uint64_t raw_gyro_data = 0;
+	uint16_t x_data;
+	uint16_t y_data;
+	uint16_t z_data;
+	i2c_multiple_read(BMI160_SLAVE_ADDR, reg_gyro_x_lo, &raw_gyro_data , 6);
+	x_data = raw_gyro_data & 0xffff;
+	y_data = (raw_gyro_data >> 16) & 0xffff;
+	z_data = (raw_gyro_data >> 32) & 0xffff;
+	data_gyro.x = x_data;
+	data_gyro.y = y_data;
+	data_gyro.z = z_data;
 
 #ifndef BMI160_PRINTF_OFF
 	PRINTF("BMI160 - Gyro en x: %d \n", data_acc.x);
